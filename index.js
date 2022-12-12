d1a71f6967af976f7f989274fbf31b77d322e29f
Billie Sheikh <bill.sheikh@bbc.co.uk>
2022-12-12 13:59:43 +0000

    sockets?

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
+
 import * as dotenv from 'dotenv';
 import dbConnector from './src/utils/dbConnector.js';
 import routes from './src/views/routes.js';
@@ -14,12 +17,21 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(routes);
 
+const server = http.createServer(app);
+const io = new Server(server);
+
 app.route('/').get((req, res) => {
-  res.send('Hey Hackathoners! 👋🏻');
+  // res.send('Hey Hackathoners! 👋🏻');
+  res.sendFile('index.html', { root: '.'});
 });
 dbConnector();
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(port, () => {
   // eslint-disable-next-line
   console.log('Server is running 🚀......');
 });
