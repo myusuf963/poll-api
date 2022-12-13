4e58c78318670a211b64cd5cb07d550989d28a6b
myusuf963 <mohammad.alcheikhmousa@bbc.co.uk>
2022-12-13 15:57:02 +0000

    Add vote endpoints and change poll schema

--- src/controllers/poll.js
+++ src/controllers/poll.js
@@ -1,8 +1,6 @@
 import Poll from '../models/Poll.js';
-// import { server } from '../utils/server.js';
-// import createIOServer from '../utils/createIOServer.js';
 
-export const createPoll = async (req, res) => {
+export const createPoll = async (req, res, next) => {
   const pollToCreate = req.body;
   const poll = new Poll(pollToCreate);
   try {
@@ -12,19 +10,22 @@ export const createPoll = async (req, res) => {
     res.status(201).json({ poll });
   } catch (error) {
     res.status(409).json({ message: error.message });
+    next(error);
   }
 };
 
-export const getPolls = async (_req, res) => {
+export const getPolls = async (req, res, next) => {
   try {
     const polls = await Poll.find();
     return res.status(200).json({ polls });
   } catch (error) {
     res.status(404).json({ message: error.message });
+    next(error);
   }
 };
 
-export const getPollsByCategory = async (req, res) => {
+export const getPollsByCategory = async (req, res, next) => {
+  res.send('getPollsByCategory is ' + req.params.category);
   try {
     const { category } = req.params;
     const polls = await Poll.find({
@@ -33,15 +34,44 @@ export const getPollsByCategory = async (req, res) => {
     return res.status(200).json({ polls });
   } catch (error) {
     res.status(404).json({ message: error.message });
+    next(error);
   }
 };
 
-export const getPoll = async (req, res) => {
+export const getPoll = async (req, res, next) => {
   try {
     const { id } = req.params;
     const poll = await Poll.findById(id);
     return res.status(200).json({ poll });
   } catch (error) {
     res.status(404).json({ message: error.message });
+    next(error);
+  }
+};
+
+export const makeVote = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { option } = req.body;
+    const poll = await Poll.findById(id);
+    const results = poll.results;
+    // if (option) {
+    //   await poll.save();
+    // }
+    const optionToUpdate = results.find((result) => result.option === option);
+    if (optionToUpdate) {
+      optionToUpdate.votes += 1;
+    } else {
+      results.push({ option, votes: 1 });
+    }
+    const updatedPoll = await Poll.findByIdAndUpdate(
+      id,
+      { results },
+      { new: true }
+    );
+    return res.status(200).json({ poll: updatedPoll });
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+    next(error);
   }
 };
