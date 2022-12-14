a0e4b5aea859f794458fa68eb12cb30092cec61f
Billie Sheikh <bill.sheikh@bbc.co.uk>
2022-12-14 11:29:14 +0000

    made global server array

---
+++ src/utils/ioServer.js
@@ -0,0 +1,37 @@
+import { Server } from 'socket.io';
+
+let servers = [];
+
+export function createIOServer(id, server) {
+    const io = new Server(server, {
+        path: id
+    });
+    if (servers.length > 0) servers = [];
+    servers.push(io);
+
+}
+
+export function runServer() {
+  [ io ] = servers;
+  io.on('connection', (socket) => {
+    console.log(`user: ${socket.id} connected`);
+  
+    socket.on('vote', (voteData) => {
+      console.log('dta', voteData);
+      socket.emit('response', voteData);
+      socket.broadcast.emit('response', voteData);
+    });
+  
+    socket.on('disconnect', () => {
+      console.log(`${socket.id} disconnected`);
+    });
+  });
+}
+
+export function deleteServers() {
+  servers = [];
+}
+
+export function getServer() {
+  return servers[0];
+}
